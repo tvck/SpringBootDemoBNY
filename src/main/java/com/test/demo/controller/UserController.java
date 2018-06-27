60a75cee4867527f05790378974a474fef16f593
Lingbo Kou <koukoukou@me.com>
2018-06-27 00:07:41 -0400

    change to lambda

--- src/main/java/com/test/demo/controller/UserController.java
+++ src/main/java/com/test/demo/controller/UserController.java
@@ -7,14 +7,10 @@ import com.test.demo.model.User;
 import com.test.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/users")
@@ -29,33 +25,30 @@ public class UserController {
     public User getUser(@PathVariable int id){
         //return userRepository.findById(id).map((e)->{return e;}).orElse(null);
         String sql="SELECT * from users,geo where users.id=geo.id and users.id="+id;
-        return jdbcTemplate.queryForObject(sql, new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                User user=new User();
-                user.setId(resultSet.getInt("id"));
-                user.setEmail(resultSet.getString("email"));
-                user.setName(resultSet.getString("name"));
-                user.setUsername(resultSet.getString("username"));
-                user.setPhone(resultSet.getString("phone"));
-                user.setWebsite(resultSet.getString("website"));
-                Address address=new Address();
-                address.setCity(resultSet.getString("city"));
-                address.setStreet(resultSet.getString("street"));
-                address.setSuite(resultSet.getString("suite"));
-                address.setZipcode(resultSet.getString("zipcode"));
-                Geo geo=new Geo();
-                geo.setLat(resultSet.getDouble("lat"));
-                geo.setLat(resultSet.getDouble("lng"));
-                address.setGeo(geo);
-                user.setAddress(address);
-                Company company=new Company();
-                company.setBs(resultSet.getString("bs"));
-                company.setCatchPhrase(resultSet.getString("catchPhrase"));
-                company.setName(resultSet.getString("company_name"));
-                user.setCompany(company);
-                return user;
-            }
+        return jdbcTemplate.queryForObject(sql, (resultSet, i) -> {
+            User user=new User();
+            user.setId(resultSet.getInt("id"));
+            user.setEmail(resultSet.getString("email"));
+            user.setName(resultSet.getString("name"));
+            user.setUsername(resultSet.getString("username"));
+            user.setPhone(resultSet.getString("phone"));
+            user.setWebsite(resultSet.getString("website"));
+            Address address=new Address();
+            address.setCity(resultSet.getString("city"));
+            address.setStreet(resultSet.getString("street"));
+            address.setSuite(resultSet.getString("suite"));
+            address.setZipcode(resultSet.getString("zipcode"));
+            Geo geo=new Geo();
+            geo.setLat(resultSet.getDouble("lat"));
+            geo.setLat(resultSet.getDouble("lng"));
+            address.setGeo(geo);
+            user.setAddress(address);
+            Company company=new Company();
+            company.setBs(resultSet.getString("bs"));
+            company.setCatchPhrase(resultSet.getString("catchPhrase"));
+            company.setName(resultSet.getString("company_name"));
+            user.setCompany(company);
+            return user;
         });
 
     }
